e5f5c8a50b3f3d1cb3d1fcabefafda2885cf3f56
RyanHTL <2388092952@qq.com>
2020-09-14 11:40:50 +0800

    Index.vue base

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -25,17 +25,6 @@ export default {
   },
   methods: {
     login() {
-      // this.$axios
-      //   .post('/login', {
-      //     username: this.loginForm.username,
-      //     password: this.loginForm.password
-      //   })
-      //   .then(successResponse => {
-      //     if (successResponse.data.code === 200) {
-      //       this.$router.replace({ path: '/index' });
-      //     }
-      //   })
-      //   .catch(failResponse => {});
       this.$router.push('/index');
     }
 
