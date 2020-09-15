9da41b6f14b77fc0d460c4eff67c15950dc2ac07
RyanHTL <2388092952@qq.com>
2020-09-15 15:45:08 +0800

    Index.vue update 1.0

--- src/components/content/nav/Nav.vue
+++ src/components/content/nav/Nav.vue
@@ -7,10 +7,18 @@
           <div>MAX评教系统</div>
         </div>
       </el-col>
-      <el-col :span="11" >
-        <div class="grid-content bg-mid">
-          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-          <p>张三</p>
+      <el-col :span="11">
+        <div class="grid-content">
+          <el-dropdown class="bg-mid">
+            <div  class="bg-mid el-dropdown-link">
+              <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+              <p>张三</p>
+            </div>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>我的信息</el-dropdown-item>
+              <el-dropdown-item>我的通知</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </div>
       </el-col>
       <el-col :span="5" class="navdel">
@@ -30,12 +38,14 @@
   #nav {
     background-color: #0a408a;
     color: #ffffff;
+    padding-bottom: 5px;
+    padding-top: 5px;
   }
 
   .navel {
     width: 68.75rem /* 1100/16 */;
-    /*background-color: #8a6771;*/
     margin: 0 auto;
+    user-select: none;
   }
 
   .navdel {
@@ -59,17 +69,23 @@
     width: 3.75rem /* 60/16 */;
     height: 3.75rem /* 60/16 */;
     float: left;
-    margin-right: 0.625rem  /* 10/16 */;
+    margin-right: 0.625rem /* 10/16 */;
   }
-  .bg-mid{
+
+  .bg-mid {
     display: flex;
     justify-content: flex-end;
     align-items: center;
   }
-  .bg-mid p{
-    /*background: #ffffff;*/
+
+  .bg-mid p {
     height: 3.75rem /* 60/16 */;
-    margin: 0 0.625rem  /* 10/16 */;
+    margin: 0 0.625rem /* 10/16 */;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #ffffff;
   }
 
   .bg-logout {
@@ -80,6 +96,10 @@
     text-align: center;
     border-radius: 3px;
     margin: 0 auto;
+    cursor: default;
+  }
+  .bg-logout:active{
+    background-color: #10a4e4;
   }
 
 </style>
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,69 +1,30 @@
 <template>
   <el-container>
     <el-header><Nav></Nav></el-header>
+    <div>
+      <pic-slide></pic-slide>
+    </div>
     <div class="el-container container">
       <el-main>Main</el-main>
       <el-aside width="200px">Aside</el-aside>
     </div>
-
     <el-footer>Footer</el-footer>
   </el-container>
 </template>
 
 <script>
-  import Nav from "../../components/content/nav/Nav.vue";
+  import Nav from "components/content/nav/Nav.vue";
+  import PicSlide from "components/content/index/slide/PicSlide";
 
   export default {
     name: 'Index',
     components: {
-      Nav
+      Nav,
+      PicSlide
     }
   };
 </script>
 
 <style scoped>
-  body {
-    min-height: 100%;
-  }
-
-  .el-header,
-  .el-footer {
-    width: 100%;
-    /*background-color: #b3c0d1;*/
-    color: #333;
-    text-align: left;
-    line-height: 60px;
-    padding: 0;
-  }
-
-  .el-footer {
-    flex: 1;
-  }
-
-  .el-aside {
-    background-color: #d3dce6;
-    color: #333;
-    height: 400px;
-    text-align: center;
-    line-height: 200px;
-  }
-
-  .el-main {
-    background-color: #e9eef3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-
-  body > .el-container {
-    margin-bottom: 40px;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .container {
-    width: 68.75rem  /* 1100/16 */;
-    height: 75rem  /* 1200/16 */;
-    margin: 100px auto;
-  }
+  @import url("~@/assets/css/index.css");
 </style>
