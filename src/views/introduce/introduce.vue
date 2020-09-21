334ac6b94c5b9d1424895044bae95b43dd154a47
misszhouhaiyang <1507392540@qq.com>
2020-09-21 12:04:25 +0800

    add introduce

---
+++ src/views/introduce/introduce.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "introduce.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/mess/Mess.vue
+++ src/views/mess/Mess.vue
@@ -4,6 +4,16 @@
       <header>
         <Nav></Nav>
       </header>
+      <div>
+        <!-- Using modifiers -->
+        <b-button v-b-modal.my-modal>Show Modal</b-button>
+
+        <!-- Using value -->
+        <b-button v-b-modal="'my-modal'">Show Modal</b-button>
+
+        <!-- The modal -->
+        <b-modal id="my-modal">Hello From My Modal!</b-modal>
+      </div>
       <!--<table class="msg">
         <tr>
           <th><span>姓名:</span>{{tableData.name}}</th>
