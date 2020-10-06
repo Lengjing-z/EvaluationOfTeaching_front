e3f636b74a4e74b92b211c37e645ada7f3caf3aa
2691032513 <2691032513@qq.com>
2020-10-06 17:48:26 +0800

    complete power-management

--- src/store/admin/power/index.js
+++ src/store/admin/power/index.js
@@ -1,14 +1,23 @@
 import myAxios from "../../../network/request";
 import post from "../../util";
+import qs from 'qs'
 const baseUrl = 'admin/power/'
 export default {
   namespaced:true,
   state:{
-    all:[]
+    all:[],
+    query:[],
+    update:[]
   },
   mutations:{
     updateAll(state,data){
       state.all = data
+    },
+    updateQuery(state,data){
+      state.query = data
+    },
+    updateUp(state,data){
+      state.update = data
     }
   },
   actions:{
@@ -16,8 +25,23 @@ export default {
       return post(baseUrl+'all',{},res=>{
         commit('updateAll',res.data)
       })
+    },
+    loadQuery({commit},condition){
+      return post(baseUrl+'query',condition,res=>{
+        commit('updateQuery',res.data)
+      })
+    },
+    updateUserPowers({commit,state}){
+      let del = state.query.filter( item=> !state.update.some(up=>up.id == item.id))
+      let add = state.update.filter(item=>item.id === undefined)
+      return Promise.all([
+        post(baseUrl+'del',del,res=>res.data),
+        post(baseUrl+'add',add,res=>res.data)
+      ])
     }
   },
   modules:{},
-  getters:{}
+  getters:{
+    
+  }
 }
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,32 +1,31 @@
 <template>
-  <div>
-<!--    导航栏-->
+<div>
+    <!--    导航栏-->
     <NavBar></NavBar>
     <aside class="slide d-none d-md-block">
-      <!--  图片轮播   -->
-      <pic-slide></pic-slide>
+        <!--  图片轮播   -->
+        <pic-slide></pic-slide>
     </aside>
     <!--  main start  -->
     <main>
-      <div class="container">
-        <div class="row">
-          <div class="span10 col-md-12 col-lg-9">
-            <!--主体内容-->
-            <function-list></function-list>
-          </div>
-          <div class="span2 d-none d-lg-block ">
-            <!--边栏内容-->
-          </div>
+        <div class="container">
+            <div class="row">
+                <div class="span10 col-md-12 col-lg-9">
+                    <!--主体内容-->
+                    <function-list></function-list>
+                </div>
+                <div class="span2 d-none d-lg-block ">
+                    <!--边栏内容-->
+                </div>
+            </div>
         </div>
-      </div>
-      <aside class="w-25"></aside>
+        <aside class="w-25"></aside>
     </main>
     <!--    main end -->
     <!--    footer stard -->
     <Footer></Footer>
 
-  </div>
-
+</div>
 </template>
 
 <script>
@@ -35,43 +34,47 @@ import PicSlide from "components/content/index/slide/PicSlide";
 import Footer from "../../components/content/footer/Footer";
 import Function from "components/common/function/Function";
 import FunctionList from "../../components/content/index/funlist/FunctionList";
+import {mapGetters} from 'vuex'
 
 export default {
-  name: 'Index',
-  components: {
-    NavBar,
-    PicSlide,
-    Footer,
-    Function,
-    FunctionList
-  },
-  data(){
-    return {
+    name: 'Index',
+    components: {
+        NavBar,
+        PicSlide,
+        Footer,
+        Function,
+        FunctionList
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        // ...mapGetters({
+        //     currentQuery: "admin/power/currentQuery"
+        // })
+    },
+    mounted() {
+        this.$store.dispatch('admin/power/loadAll')
+        this.$store.dispatch('admin/power/loadQuery', {uId:32}).then(() => {
+            window.console.log(this.$store.state.admin.power.query)
+        })
+        // let data = [{
+        //     id:23,
+        //     pid:2,
+        //     uid:32
+        // },{
+        //     pid:1,
+        //     uid:32
+        // },{
+        //     pid:5,
+        //     uid:32
+        // }]
+        // this.$store.commit('admin/power/updateUp',data)
+        // this.$store.dispatch('admin/power/updateUserPowers').then(res=>window.console.log(res))
 
     }
-  },
-  mounted() {
-    let data = [
-      {
-      name:'add1',
-      pub: true
-    },{
-      name:'add2',
-      pub: true
-    },{
-      name:'add3',
-      pub: true
-    },{
-      name:'add4',
-      pub: true
-    }]
-    this.$store.commit('admin/class/addCreations',data)
-    this.$store
-      .dispatch('admin/class/create')
-      .then(res=>{
-        window.console.log(res)
-      })
-  }
 };
 </script>
 
