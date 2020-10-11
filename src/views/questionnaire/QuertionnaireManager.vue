f6e0f171aa96272f9b62d92dc8486c9b138e8037
misszhouhaiyang <1507392540@qq.com>
2020-10-11 19:42:59 +0800

    合并1.0

---
+++ src/views/questionnaire/QuertionnaireManager.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <manager-setting :manager = "manager"></manager-setting>
+    <Questionnaire></Questionnaire>
+
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/content/nav/NavBar";
+import Footer from "components/content/footer/Footer";
+import ManagerSetting from "components/common/Manager/ManagerSetting";
+import Questionnaire from "components/content/questionnaire/Questionnaire";
+
+export default {
+  name: "QuertionnaireManager",
+  data(){
+    return{
+      manager: [{name:'问卷管理'}]
+    }
+  },
+  components: {
+    NavBar,
+    Footer,
+    ManagerSetting,
+    Questionnaire
+  },
+  computed:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
