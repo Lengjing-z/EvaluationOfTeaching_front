1980b1394046c694ddfeb81f97f8b3354aa0de8e
2691032513 <2691032513@qq.com>
2020-10-19 22:23:48 +0800

    sector diagram data analyse

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template id="index">
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <script>
@@ -21,12 +21,26 @@ export default {
     window.addEventListener("beforeunload", () => {
       sessionStorage.setItem("store", JSON.stringify(this.$store.state))
     })
+  },
+  methods: {
+      generateDiagramData(indecators, answers){
+        let data = []
+        (function dfs(indecators, answers,children){
+          indecators.forEach(indcator=>{
+            children.push({
+              name: indcator,
+              value:answers.filter(item=>indcator.indexId).reduce((total,current)=>total+Number(current.answer))*indcator.rate,
+              children:[]
+            })
+            dfs(indcator.children,answers,children[children.length-1].children)
+          })
+        })(indecators,answers,data)
+        return data
+      }
   }
 }
 </script>
-
-
+- 
 <style>
 @import url("./assets/css/base.css");
-
 </style>
