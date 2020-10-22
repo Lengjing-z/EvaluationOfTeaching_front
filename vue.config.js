681f48a7e20906144af106131388627a3ab6beae
RyanHTL <55087067+RyanHTL@users.noreply.github.com>
2020-10-22 16:17:24 +0800

    Update vue.config.js
    change target

--- vue.config.js
+++ vue.config.js
@@ -22,9 +22,9 @@ module.exports = {
     port: 8080,
     proxy: {
       'back/': {
-        // target: 'http://121.89.205.249:9090',
+         target: 'http://121.89.205.249:9090',
         // target: 'http://192.168.43.2:9090',
-        target: 'http://192.168.192.1:9090',
+        // target: 'http://192.168.192.1:9090',
         // target: 'http://29175vv342.zicp.vip',
         changeOrigin: true,
         pathRewrite: {
